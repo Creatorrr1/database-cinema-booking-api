24f8f5a686f7377b9ceccfc00b7119cfe9b43754
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-06-10 17:48:46 +0100

    Requirements and Extensions 1,2

--- src/controllers/movieList.js
+++ src/controllers/movieList.js
@@ -1,27 +1,59 @@
+const { movie } = require("../utils/prisma");
 const prisma = require("../utils/prisma");
 
+// map
+const filters = {
+  greaterThan: "gt",
+  lessThan: "lt",
+};
+
 const getMovies = async (req, res) => {
-  const createdMovies = await prisma.movie.findMany({
+  // get data from the request
+  const { runtimeMins, comparison } = req.query;
+
+  // figure out is it less or greater than
+
+  // only add the where clause if request has the query parameters
+
+  const whereData = {};
+
+  if (runtimeMins && comparison) {
+    const filter = {};
+    const prismaOperator = filters[comparison];
+    filter[prismaOperator] = Number(runtimeMins);
+    whereData.runtimeMins = filter;
+  }
+
+  const movies = await prisma.movie.findMany({
+    where: whereData,
     include: {
       screenings: true,
     },
   });
-
-  res.json({ movies: createdMovies });
+  res.json({ data: movies });
 };
 
 const addMovieDetails = async (req, res) => {
-  //   const { title, runtimeMins } = req.body;
-  const createdMovie = await prisma.movie.create({
-    data: {
-      //   title,
-      //   runtimeMins
-      title: req.body.title,
-      runtimeMins: Number(req.body.runtimeMins),
-    },
-  });
+  // get data from request - req.body
+  const { screenings } = req.body;
 
-  res.json({ data: createdMovie });
+  const movieData = {
+    title: req.body.title,
+    runtimeMins: Number(req.body.runtimeMins),
+  };
+
+  if (screenings) {
+    movieData.screenings = { create: screenings };
+  }
+
+  try {
+    const movie = await prisma.movie.create({
+      data: movieData,
+    });
+    res.json({ data: movie });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
 };
 
 const getMovieByID = async (req, res) => {
